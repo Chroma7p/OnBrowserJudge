545ef4939a03a21c9582ec28f28689c5bb2b3d11
chroma7p <nahco3.hcl@gmail.com>
2022-05-15 17:28:08 +0900

    ジャッジコードの埋め込みなおし

--- js/testcasemaker.js
+++ js/testcasemaker.js
@@ -1,5 +1,4 @@
-//参考: https://github.com/pyodide/pyodide/issues/8
-import pyodide from "https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js"
+
 class Item {
     constructor(name, value) {
     this.name = name
@@ -171,63 +170,3 @@ if (out != "") console.log(out)
     }
 */
 
-
-
-function setup_pyodide() {
-        // setup pyodide environment to run code blocks as needed
-        var setup_code = `
-import sys, io, traceback
-namespace = { }  # use separate namespace to hide run_code, modules, etc.
-def run_code(code):
-"""run specified code and return stdout and stderr"""
-out = io.StringIO()
-oldout = sys.stdout
-olderr = sys.stderr
-sys.stdout = sys.stderr = out
-try:
-# change next line to exec(code, { }) if you want to clear vars each time
-exec(code, namespace)
-except:
-traceback.print_exc()
-sys.stdout = oldout
-sys.stderr = olderr
-return out.getvalue()
-`
-pyodide.runPython(setup_code)
-}
-document.getElementById("submit-button").onclick = function () {
-    // run code currently stored in editor
-    pyodide.globals.code_to_run = editor.getSession().getValue()
-        //console.log(pyodide.globals.code_to_run)
-        let judge=[]
-let AC=0
-for (const i in testcase){
-    //console.log(answers[i])
-    pyodide.globals.testcase = testcase[i]
-            const result=pyodide.runPython(`
-import io,sys
-sys.stdin=io.StringIO(testcase)
-run_code(code_to_run)
-`).split("\n")
-const answer=answers[i].split("\n")
-//console.log(result)
-let flag=true
-for (const j in answer){
-    console.log(result[j])
-                console.log(answer[j])
-if (result[j]!=answer[j]){
-    flag = false
-                    break
-                }
-            }
-if (flag) {
-    judge.push("AC")
-                AC++
-            }
-else judge.push("WA")
-        }
-document.getElementById("output").value=`${AC}/${testcase.length} ${ACjudge(AC, testcase.length)}`
-        
-    }
-// run setup_pyodide() when pyodide finishes loading
-languagePluginLoader.then(setup_pyodide)
