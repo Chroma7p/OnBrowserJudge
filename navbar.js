f70fe77a4d984d40106f578436ea0f68b2ee98ed
chroma7p <nahco3.hcl@gmail.com>
2022-05-15 13:23:48 +0900

    classNameになってない場所を修正

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ const navbar = ReactDOM.createRoot(document.getElementById('navbar'));
 navbar.render(
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a href="index.html" className="navbar-brand">STP Judge</a>
-        <ul class="navbar-nav">
+        <ul className="navbar-nav">
             <li className="nav-item">
                 <a href="index.html" className="nav-link" >トップ</a>
             </li>
@@ -10,7 +10,7 @@ navbar.render(
                 <a href="codetest.html" className="nav-link">コードテスト</a>
             </li>
             <li className="nav-item">
-                <a href="https://github.com/MogamiTsuchikawa/ShoppingThemeProgram/blob/master/Python/%E5%95%8F%E9%A1%8C%E6%96%87.md" class="nav-link" target="blank">ルール</a>
+                <a href="https://github.com/MogamiTsuchikawa/ShoppingThemeProgram/blob/master/Python/%E5%95%8F%E9%A1%8C%E6%96%87.md" className="nav-link" target="blank">ルール</a>
             </li>
             <li className="nav-item">
             <a href="https://github.com/Chroma7p/OnBrowserJudge" className="nav-link" target="blank">GitHub</a>
