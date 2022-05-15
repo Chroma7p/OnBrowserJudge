78ffda4a0832a68f6fb8ad7e48607f5eab2bfedf
chroma7p <nahco3.hcl@gmail.com>
2022-05-15 13:43:09 +0900

    変数名の変更忘れを修正

--- editor.js
+++ editor.js
@@ -1,5 +1,5 @@
-const navbar = ReactDOM.createRoot(document.getElementById('editorarea'));
-navbar.render(
+const edt = ReactDOM.createRoot(document.getElementById('editorarea'));
+edt.render(
     <div>
     <div id="editor" style="height: 300px;width:600px"></div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ace.js"></script>
