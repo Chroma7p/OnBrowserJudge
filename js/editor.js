286ac66b3369c2c1bb7bfa81b73e2eba3922eb7f
chroma7p <nahco3.hcl@gmail.com>
2022-05-15 14:42:19 +0900

    エディタが動かない問題を修正

--- js/editor.js
+++
@@ -1,6 +0,0 @@
-var editor = ace.edit("editor");
-//editor.setTheme("ace/theme/monokai");
-editor.setFontSize(14);
-editor.getSession().setMode("ace/mode/python");
-editor.getSession().setTabSize(4);
-editor.$blockScrolling = Infinity;
