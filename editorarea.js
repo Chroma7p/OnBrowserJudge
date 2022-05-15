713bf3b7b09d8c02b4508672b127836e11536fd1
chroma7p <nahco3.hcl@gmail.com>
2022-05-15 13:51:02 +0900

    エディタの圧縮を妥協

--- editorarea.js
+++
@@ -1,8 +0,0 @@
-const edt = ReactDOM.createRoot(document.getElementById('editorarea'));
-edt.render(
-    <div>
-    <div id="editor" style="height: 300px;width:600px"></div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ace.js"></script>
-    <script src="editor.js"></script>
-    </div>
-);
